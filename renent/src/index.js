cfb1d03d1b777cfc1ff29e99539c91a6705d20dd
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-01-09 22:15:22 +0500

    10.15

--- renent/src/index.js
+++ renent/src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+
+    <p>
+      Do subscribe me
+    </p>
+    <ol>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ol>
+  </>
+
+  ,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
