4e76b127d1e2d9e4fb30fb75911d795c9a648aa8
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-01-20 13:42:14 +0500

    challenge8Complete

--- challenge-8/src/App.js
+++ challenge-8/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const change = (event) => {
+
+    if (event.target.id === "inc") {
+
+      setCount(count + 1);
+    } else {
+
+      if (count > 0) {
+        setCount(count - 1);
+      }
+      else {
+        alert("Limit reached to  0");
+        setCount(0);
+      }
+
+
+
+    }
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Challenge 8
+        <div>
+          <h1>{count}</h1>
+          <button onClick={change} id="inc">Increm</button>
+          <button onClick={change} id="dec">Decrem</button>
+        </div>
+
       </header>
     </div>
   );
