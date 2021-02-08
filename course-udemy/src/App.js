e2225e788b971bea66a1c535ef7851bc876b3e9c
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-02-08 10:41:19 +0500

    dynamic styles

--- course-udemy/src/App.js
+++ course-udemy/src/App.js
@@ -46,30 +46,46 @@ function App() {
     setPeople(person);
   }
 
+  const style = {
+    color: "white",
+    font: "inherit",
+    border: "1px solid green",
+    padding: "8px",
+    cursor: "pointer",
+    backgroundColor: "green"
+  }
+
+  let person = null;
+  if (toggle === true) {
+    person = <div>
+
+      {people.map((person, index) => {
+        return <People name={person.name}
+          age={person.age}
+          click={() => deletePerson(index)}
+          key={index}
+          change={(event) => {
+            changeState(event, person.id)
+          }} />
+      })}
+
+      {/* <People name={people[0].name}
+    click={changeState.bind(this, "New Murshad")} >Ok then</People>
+  <People age={people[1].age} >{people[1].name}</People>
+  <People name={people[2].name} age={people[2].age} /> */}
+    </div>;
+    style.backgroundColor = "red";
+    style.border = "1px solid red";
+
+  }
+
   return (
     <div className="App">
-      <button onClick={toggleChange}>Toggle State</button>
+      <h1>Hi I'm React</h1>
+      <hr />
+      <button onClick={toggleChange} style={style}>Toggle State</button>
+      {person}
 
-      {toggle ?
-        <div>
-
-          {people.map((person, index) => {
-            return <People name={person.name}
-              age={person.age}
-              click={() => deletePerson(index)}
-              key={index}
-              change={(event) => {
-                changeState(event, person.id)
-              }} />
-          })}
-
-          {/* <People name={people[0].name}
-            click={changeState.bind(this, "New Murshad")} >Ok then</People>
-          <People age={people[1].age} >{people[1].name}</People>
-          <People name={people[2].name} age={people[2].age} /> */}
-        </div>
-        : null
-      }
     </div>
   );
 }
