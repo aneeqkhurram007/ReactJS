402215658d19a3f20826ba6ecd630b75cbf21448
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-02-08 19:21:24 +0500

    dynamic styling

--- course-udemy/src/App.js
+++ course-udemy/src/App.js
@@ -6,13 +6,13 @@ import styled from 'styled-components'
 const StyledButton = styled.button`
     color: white;
     font: inherit;
-    border: 1px solid green;
+    border: 1px solid ${props => props.alt ? 'red' : 'green'};
     padding: 8px;
     cursor: pointer;
-    background-color: green;
+    background-color: ${props => props.alt ? 'red' : 'green'};
     
     &:hover{
-      background-color: lightgreen;
+      background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
       color: black;
     }
 
@@ -106,7 +106,7 @@ function App() {
       <h1>Hi I'm React</h1>
       <p className={classes.join(' ')}>This is really working</p>
       <hr />
-      <StyledButton onClick={toggleChange} >Toggle State</StyledButton>
+      <StyledButton alt={toggle} onClick={toggleChange} >Toggle State</StyledButton>
       {person}
 
     </div>
