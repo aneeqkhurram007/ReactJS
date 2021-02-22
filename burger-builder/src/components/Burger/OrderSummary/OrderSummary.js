c3dfbd5aa00dda59b57be1022047b8080bff9c1f
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-02-22 11:01:17 +0500

    Folder Structure Improved

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import React, { Component } from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
