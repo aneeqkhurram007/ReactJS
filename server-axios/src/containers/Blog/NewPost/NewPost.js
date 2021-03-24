1341a49bd47b4042b3f70b7ece464d29b482f3aa
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-03-24 20:45:02 +0500

    Guards

--- server-axios/src/containers/Blog/NewPost/NewPost.js
+++ server-axios/src/containers/Blog/NewPost/NewPost.js
@@ -12,6 +12,7 @@ class NewPost extends Component {
     }
 
     componentDidMount() {
+
         console.log(this.props);
     }
 
@@ -24,7 +25,8 @@ class NewPost extends Component {
         axios.post("/posts", data)
             .then(response => {
                 console.log(response);
-                this.setState({ submitted: true });
+                this.props.history.replace('/posts');
+                // this.setState({ submitted: true });
             })
     }
     render() {
