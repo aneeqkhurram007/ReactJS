83d81556d54c70e898a7ebd704101de22731938e
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-03-25 06:17:34 +0500

    dynamic loading

--- server-axios/src/containers/Blog/Blog.js
+++ server-axios/src/containers/Blog/Blog.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './Blog.css';
-import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import Posts from './Posts/Posts';
+// import NewPost from './NewPost/NewPost';
+import aSyncComponent from '../../hoc/aSynComponent/aSyncComponent';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
+const AsyncNewPost = aSyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 class Blog extends Component {
 
     state = {
@@ -34,7 +38,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null}
+                    {this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null}
                     <Route path='/posts' component={Posts} />
                     <Route render={() => <h1>Not found</h1>} />
                     {/* <Redirect from='/' to="/posts" /> */}
