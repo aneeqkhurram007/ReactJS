0aad85f04e6bbd280f4054da7deebf271e3cccbb
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-04-10 09:37:57 +0500

    lec 7

--- burger-builder-3/src/containers/Checkout/Checkout.js
+++ burger-builder-3/src/containers/Checkout/Checkout.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom';
+import ContactData from '../Checkout/ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 class Checkout extends Component {
     state = {
@@ -31,6 +33,9 @@ class Checkout extends Component {
                     checkoutContinued={this.checkoutContinuedHandler}
 
                 />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+
+
             </div>
         )
     }
