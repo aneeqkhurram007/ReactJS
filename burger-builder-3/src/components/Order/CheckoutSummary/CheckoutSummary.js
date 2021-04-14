31932915c78288ac40286c6c1355c4ee2902ced0
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-04-14 22:59:35 +0500

    10.59

--- burger-builder-3/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder-3/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -4,7 +4,7 @@ import Burger from '../../Burger/Burger';
 import classes from './CheckoutSummary.css';
 const checkoutSummary = (props) => {
     return (
-        <div class={classes.CheckoutSummary}>
+        <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
             <div style={{
                 width: '100%',
