9d681ed4c2464d89ed3a28078fd0684e71d10673
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-07-14 21:49:46 +0500

    18.4

--- burger-builder-3/src/App.js
+++ burger-builder-3/src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 class App extends Component {
   render() {
     return (
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/orders" exact component={Orders} />
+          <Route path="/auth" exact component={Auth} />
           <Route path="/checkout" component={Checkout} />
         </Layout>
       </div>
