347d7767cdc0be2031d773240d349635da54f36a
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-07-24 22:11:55 +0500

    modifications

--- React Hooks/stateChange/change-state/src/App.js
+++ React Hooks/stateChange/change-state/src/App.js
@@ -5,19 +5,19 @@ function App() {
   let name = "Aneeq Khurram";
   let modified = 'Modified';
   const [state, setState] = useState(name);
+  const changeMyName = () => {
 
+    let temp = (state === name) ? modified : name;
+    setState(temp);
+    modified = name;
+    name = state;
+  }
   return (
-    <div className="App">
+    <div className="App-header">
       <h1>
         {state}
       </h1>
-      <button onClick={() => {
-
-        let temp = (state === name) ? modified : name;
-        setState(temp);
-        modified = name;
-        name = state;
-      }} >
+      <button onClick={changeMyName} >
         Click Me
       </button>
 
