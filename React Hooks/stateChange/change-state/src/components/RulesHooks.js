5fc38627839a29b98d3053ad457f9a35e1d71438
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-07-24 22:09:15 +0500

    10.09

---
+++ React Hooks/stateChange/change-state/src/components/RulesHooks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const RulesHook = (props) => {
+
+    return (
+        <div>
+            <h1>
+                {props.state}
+            </h1>
+        </div>
+    )
+}
+export default RulesHook;
