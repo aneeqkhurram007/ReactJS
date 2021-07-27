37de48a5f4e38791140f0fb4bd2a94f4f019fd68
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-07-27 14:16:47 +0500

    Project 1

--- React Hooks/stateChange/change-state/src/components/useEffect/Project1.js
+++ React Hooks/stateChange/change-state/src/components/useEffect/Project1.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Project1.css'
 const Project1 = () => {
 
+    const [users, setusers] = useState([])
+    const getUsers = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/albums/1/photos');
+        setusers(await response.json());
+
+    }
+    useEffect(() => {
+        getUsers();
+    }, [])
     return (
         <>
             <h2>List of GitHub Users</h2>
-            <div class="card mb-3" style={{ maxWidth: "540px" }}>
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="..." />
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            {
+                users.map((currEl) => {
+                    const { title, id, url } = currEl;
+                    return (
+                        <div key={id} className="card mb-3" style={{ maxWidth: "540px" }}>
+                            <div className="row g-0">
+                                <div className="col-md-4">
+                                    <img src={url} className="img-fluid rounded-start" alt={title} />
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{title}</h5>
+                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-
+                    )
+                })
+            }
         </>
     )
 }
