bb945f17c8e1b2fb3cc4ecff9e1e0ca3013862ac
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-08-10 20:33:54 +0500

    8.33

---
+++ React Hooks/portfolio/src/TabMenu/menu.js
@@ -0,0 +1,45 @@
+const menu = [
+    {
+        id: 1,
+        name: "maggi",
+        category: "breakfast",
+        price: "12",
+        description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit.Rerum eos ipsa ea placeat minus nemo esse sed debitis doloremque iste nam, ratione itaque in facere odit illo deleniti fuga reprehenderit?"
+    }, {
+        id: 2,
+        name: "noodles",
+        category: "dinner",
+        price: "12",
+        description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit.Rerum eos ipsa ea placeat minus nemo esse sed debitis doloremque iste nam, ratione itaque in facere odit illo deleniti fuga reprehenderit?"
+    }
+    , {
+        id: 3,
+        name: "aaloo",
+        category: "brunch",
+        price: "12",
+        description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit.Rerum eos ipsa ea placeat minus nemo esse sed debitis doloremque iste nam, ratione itaque in facere odit illo deleniti fuga reprehenderit?"
+    }
+    , {
+        id: 4,
+        name: "pakora",
+        category: "dinner",
+        price: "12",
+        description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit.Rerum eos ipsa ea placeat minus nemo esse sed debitis doloremque iste nam, ratione itaque in facere odit illo deleniti fuga reprehenderit?"
+    }
+    , {
+        id: 5,
+        name: "anda",
+        category: "evening",
+        price: "12",
+        description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit.Rerum eos ipsa ea placeat minus nemo esse sed debitis doloremque iste nam, ratione itaque in facere odit illo deleniti fuga reprehenderit?"
+    }
+    , {
+        id: 6,
+        name: "paratha",
+        category: "breakfast",
+        price: "12",
+        description: " Lorem ipsum dolor sit, amet consectetur adipisicing elit.Rerum eos ipsa ea placeat minus nemo esse sed debitis doloremque iste nam, ratione itaque in facere odit illo deleniti fuga reprehenderit?"
+    }
+
+];
+export default menu;
